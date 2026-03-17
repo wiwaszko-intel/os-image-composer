cf4a5ee44c9d688db172831c14d7c8e9c2d97c14
chrngc <149708414+chrngc@users.noreply.github.com>
2026-03-17 16:59:55 +0800

    Fixes for error logs when building UKI (#485)
    * Changed error messages to warnings in buildImageUKI, UKI fixes.
    
    * Formatted imageos.go with gofmt
    
    Signed-off-by: Cherng Xi Chia <cherng.xi.chia@intel.com>
    
    ---------
    
    Signed-off-by: Cherng Xi Chia <cherng.xi.chia@intel.com>

--- internal/image/imageos/imageos.go
+++ internal/image/imageos/imageos.go
@@ -1065,36 +1065,31 @@ func buildImageUKI(installRoot string, template *config.ImageTemplate) error {
 		if _, err := os.Stat(installRoot); err == nil {
 			log.Infof("Install Root Exists at %s", installRoot)
 		} else {
-			log.Errorf("Install Root does not exist at %s", installRoot)
-		}
-		if _, err := os.Stat(kernelPath); err == nil {
-			log.Infof("kernelPath  Exists at %s", kernelPath)
-		} else {
-			log.Errorf("Install Root does not exist at %s", installRoot)
+			log.Warnf("Install Root does not exist at %s", installRoot)
 		}
 
-		if _, err := os.Stat(kernelPath); err == nil {
+		if _, err := os.Stat(filepath.Join(installRoot, kernelPath)); err == nil {
 			log.Infof("kernelPath  Exists at %s", kernelPath)
 		} else {
-			log.Errorf("kernelPath does not exist at %s", kernelPath)
+			log.Warnf("kernelPath does not exist at %s", kernelPath)
 		}
 
-		if _, err := os.Stat(initrdPath); err == nil {
+		if _, err := os.Stat(filepath.Join(installRoot, initrdPath)); err == nil {
 			log.Infof("initrdPath  Exists at %s", initrdPath)
 		} else {
-			log.Errorf("initrdPath does not exist at %s", initrdPath)
+			log.Warnf("initrdPath does not exist at %s", initrdPath)
 		}
-		if _, err := os.Stat(cmdlineFile); err == nil {
+
+		if _, err := os.Stat(filepath.Join(installRoot, cmdlineFile)); err == nil {
 			log.Infof("cmdlineFile  Exists at %s", cmdlineFile)
-			return nil
 		} else {
-			log.Errorf("cmdlineFile does not exist at %s", cmdlineFile)
+			log.Warnf("cmdlineFile does not exist at %s", cmdlineFile)
 		}
-		if _, err := os.Stat(outputPath); err == nil {
+
+		if _, err := os.Stat(filepath.Join(installRoot, outputPath)); err == nil {
 			log.Infof("outputPath  Exists at %s", outputPath)
-			return nil
 		} else {
-			log.Errorf("outputPath does not exist at %s", outputPath)
+			log.Warnf("outputPath does not exist at %s", outputPath)
 		}
 
 		if err := buildUKI(installRoot, kernelPath, initrdPath, cmdlineFile, outputPath, template); err != nil {
